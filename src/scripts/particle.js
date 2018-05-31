be0e05bc1446a4f59d114f4e56a5ae9b51213b7e
Ivan <iv.xromov@mail.ru>
2018-05-31 21:47:36 +0300

    some changes

--- src/scripts/particle.js
+++ src/scripts/particle.js
@@ -1,30 +1,40 @@
 export default class Particle {
     constructor(initialX, initialY) {
-        this.speed = this.getRandomNumber(10, 20)
+        this.speed = Particle.getRandomNumber(50, 100)
+
+        this.currentDX = 0
+        this.currentDY = 0
 
         this.el = document.createElement('div')
         this.el.style.position = 'absolute'
-        this.el.style.transition = 'all 0.1s ease 0s'
+
+        //Изменим тип анимации на более простой, это должно помочь с тормозами
+        //Так же увеличим время анимации и интервал, через который происходит перемещение объекта.
+        // Это в 5 раз сократит количество анимированных перемещений и повысит поизводительность
+        this.el.style.transition = 'all 0.5s cubic-bezier(0, 0, 1, 1) 0s'
         this.el.style['border-radius'] = '50%'
 
         document.body.appendChild(this.el)
 
-        this.setPosition(initialX, initialY)
         this.setRandomSize()
-        this.setRandomColor()
+        //Мы хотим, чтобы частица появлялась там, где мы кликнули
+        this.setPosition(initialX - this.getPixels('width')/2, initialY - this.getPixels('height')/2)
+        this.el.style.background = Particle.getRandomColor();
         this.startBrownianMotion()
     }
 
-    getRandomNumber(from, to) {
+    //Не обязательно хранить этот метод в каждом объекте
+    static getRandomNumber(from, to) {
         const rnd = Math.random()
         return parseInt((rnd * (to - from)) + from)
     }
 
-    setRandomColor() {
+    //И метод получения цвета
+    static getRandomColor() {
         const rnd = Math.random()
         const hex = 0x1000000 + rnd * 0xffffff
         const color = hex.toString(16).substr(1, 6)
-        this.el.style.background = ['#', color].join('')
+        return ['#', color].join('')
     }
 
     getPixels(property) {
@@ -37,17 +47,9 @@ export default class Particle {
     }
 
     setRandomSize() {
-        const side = this.getRandomNumber(20, 70)
+        const side = Particle.getRandomNumber(20, 70)
         this.setPixels('width', side)
         this.setPixels('height', side)
-    }
-
-    getPositionX() {
-        return this.getPixels('left')
-    }
-
-    getPositionY() {
-        return this.getPixels('top')
     }
 
     setPosition(x, y) {
@@ -55,18 +57,41 @@ export default class Particle {
         this.setPixels('top', y)
     }
 
+    //изменять позицию будем не отступами, а с помощью transform. Это существенно повысит производительность
+    setPositionByTransform(dx, dy) {
+        this.el.style.transform = 'translate(' + dx + 'px, ' + dy + 'px)';
+    }
+
     moveRandomly() {
-        const to = parseInt(this.speed / 2)
+        const screenWidth = document.documentElement.clientWidth
+        const screenHeight = document.documentElement.clientHeight
+
+        //В зависимости от размера экрана частицы будут прыгать на разные расстояния
+        const to = parseInt(this.speed / 2) * 1000000 / (screenHeight * screenWidth)
         const from = to * -1
-        const prevX = this.getPositionX()
-        const prevY = this.getPositionY()
-        const nextX = prevX + this.getRandomNumber(from, to)
-        const nextY = prevY + this.getRandomNumber(from, to)
-        this.setPosition(nextX, nextY)
+
+        const elemRect = this.el.getBoundingClientRect()
+
+        let dX = Particle.getRandomNumber(from, to)
+        let dY = Particle.getRandomNumber(from, to)
+
+        //Это чтобы частицы не уезжали за границы экрана
+        if (dX < 0 && dX + elemRect.left < 0) dX *= -1
+        if (dX > 0 && dX + elemRect.right > screenWidth) dX *= -1
+
+        if (dY < 0 && dY + elemRect.top < 0) dY *= -1
+        if (dY > 0 && dY + elemRect.bottom > screenHeight) dY *= -1
+
+        this.currentDX += dX;
+        this.currentDY += dY;
+
+        this.setPositionByTransform(this.currentDX, this.currentDY)
     }
 
     startBrownianMotion() {
-        const timeout = 100
+        //Частицы должны начать двигаться сразу
+        this.moveRandomly()
+        const timeout = 500
         const interval = setInterval(this.moveRandomly.bind(this), timeout)
     }
 }
